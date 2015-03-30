e045ae161a77fff6f1842de8922751a2c544228c
muma378 <muma.378@163.com>
2015-03-30 17:29:52 +0100

    Initial Commit

---
+++ CVAssessments/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CVAssessments
+//
+//  Created by Xiao Yang on 15/3/30.
+//  Copyright (c) 2015年 Young. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
